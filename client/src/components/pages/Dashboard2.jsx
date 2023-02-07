f1ee81aa12a49ef7e942c79c048c46febfe58f68
Ines Alejandro Garcia Mosqueda <inesgamos@gmail.com>
2023-02-07 00:58:20 -0600

    dashboard cards

--- client/src/components/pages/Dashboard2.jsx
+++ client/src/components/pages/Dashboard2.jsx
@@ -7,7 +7,40 @@ import {
   Tab,
   TabPanel,
   TabPanels,
+  Card,
+  CardBody,
+  Image,
+  Stack,
+  Heading,
+  Text,
+  Divider,
+  CardFooter,
+  SimpleGrid,
+  ButtonGroup,
+  Button,
+  Progress,
+  Spacer,
+  
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
 } from "@chakra-ui/react";
+import { transform } from 'framer-motion';
+
+
+let cardInfoTest = {
+  "Owner" : "0x74845641561515156464",
+  "Title" : "La baina",
+  "Description" : "Una descripcion bien aca",
+  "Goal" : "Mucho dinero",
+  "DeadLine" : "Manana",
+  "Image" : "https://avatars.githubusercontent.com/u/84602829?v=4",
+
+}
 
 let categories = [
   "Tecnologia",
@@ -19,10 +52,51 @@ let categories = [
   "Otro"
 ]
 
+function DisplayCard(){
+  return(
+    <Card maxW='300px'
+    _hover={{
+      transform: "scale(1.02)",
+      transition : " all 0.4s ease-in-out",
+      cursor : "pointer", 
+    }}>
+      <CardBody>
+        <Image
+          src={cardInfoTest.Image}
+          alt={cardInfoTest.Title}
+          borderRadius='lg'
+        />
+        <Stack mt='6' spacing='3'>
+          <Heading size='md'>{cardInfoTest.Title}</Heading>
+          <Text>
+            {cardInfoTest.Description}
+          </Text>
+          <Text color='blue.600' fontSize='2xl'>
+            {cardInfoTest.Goal}
+            <Progress colorScheme='green' size='sm' value={20}/>
+            <Flex>
+              <Text fontSize="sm" color="green" as="b">1,000.00</Text>
+              <Spacer/>
+              <Text fontSize="sm" color="purple" as="b">{cardInfoTest.Goal}</Text>
+            </Flex>
+          </Text>
+        </Stack>
+      </CardBody>
+      <Divider />
+      <CardFooter>
+        <Text fontSize="sm">Alguna observacion por aqui, si es que tiene</Text>
+      </CardFooter>
+    </Card>
+  )
+}
+
 
 function DisplaySector(sector){
   return(
-    <div>{sector}</div>
+    <SimpleGrid spacing={1} templateColumns='repeat(auto-fill, minmax(200px, 1fr))'>
+      {/* <div>{sector}</div> */}
+      {DisplayCard(sector)}
+    </SimpleGrid>
   )
 }
 
@@ -45,7 +119,7 @@ const Dashboard = () => {
     <Box w="full" p="70px">
       <Flex alignContent="center" justifyContent="space-between">
         {/* DashBoard */}
-        <Tabs variant='soft-rounded' colorScheme='purple'>
+        <Tabs w="100%" variant='soft-rounded' colorScheme='purple'>
           <TabList>   {/* Categories Labels*/}
             {dashboardCategories}
           </TabList>
